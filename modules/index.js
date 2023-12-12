ba804f169d5318bd282178e519f0de188e073a69
iskariot <daosonmail@protonmail.com>
2023-12-12 19:40:55 +0200

    refactoring

--- modules/index.js
+++ modules/index.js
@@ -2,21 +2,23 @@ const { ZKSWAP, SPACEFI } = require('../utils/constants.js');
 const { swapExactUSDC, swapExactETH } = require('./swapExact.js');
 const { syncswap } = require('./syncswap.js');
 
-function getUsdcModules(signer, amount) {
+function getUsdcModule(signer, amount) {
   const swap1 = () => swapExactUSDC(signer, ZKSWAP, amount);
   const swap2 = () => swapExactUSDC(signer, SPACEFI, amount);
   const swap3 = () => syncswap(signer, amount, false);
-  return [swap1, swap2, swap3];
+  const modules = [swap1, swap2, swap3];
+  return modules[Math.floor(Math.random() * modules.length)];
 }
 
-function getEthModules(signer, amount) {
+function getEthModule(signer, amount) {
   const swap1 = () => swapExactETH(signer, ZKSWAP, amount);
   const swap2 = () => swapExactETH(signer, SPACEFI, amount);
   const swap3 = () => syncswap(signer, amount, true);
-  return [swap1, swap2, swap3];
+  const modules = [swap1, swap2, swap3];
+  return modules[Math.floor(Math.random() * modules.length)];
 }
 
 module.exports = {
-  getUsdcModules,
-  getEthModules,
+  getUsdcModule,
+  getEthModule,
 };
