37720a270fc0f389efc1d81310300cb0c6bdef75
iskariott <blablaname4@gmail.com>
2024-01-12 06:13:22 +0200

    fix random

--- utils/getSortedAccsByDays.js
+++ utils/getSortedAccsByDays.js
@@ -16,13 +16,12 @@ function round(value) {
 (() => {
   let data = moment().format('DD.MM.YY hh:mm:ss a') +  '\n' + '#'.repeat(20) + '\n'
   const accsInDay = round(ACCOUNTS / 7);
-  const accs = shuffle( Array.from(Array(ACCOUNTS).keys()))
   for (let i = 1; i <= 4; i++) {
     data+= `\nWeek ${i}\n\n`
-    let newAccs = accs;
-    for (let j = 1; j <= 7 && newAccs.length; j++) {
-      data += `Day ${j}: ${newAccs.slice(0, accsInDay).join(',')}\n`
-      newAccs = newAccs.slice(accsInDay);
+    let accs = shuffle(Array.from(Array(ACCOUNTS).keys()));
+    for (let j = 1; j <= 7 && accs.length; j++) {
+      data += `Day ${j}: ${accs.slice(0, accsInDay).join(',')}\n`
+      accs = accs.slice(accsInDay);
     }
   }
   fs.writeFileSync(path.join(__dirname, '../sortedAccs.txt'), data)
